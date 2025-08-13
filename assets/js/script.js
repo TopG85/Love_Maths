a46f011fbfdbdf3831b69d54826dfa1d22da11b4
topg85 <danielcarson1985@gmail.com>
2025-08-13 13:11:31 +0100

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,4 +139,4 @@ function displayDivisionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
-}
+} 
